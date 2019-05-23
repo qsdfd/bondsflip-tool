868ada65ba7c452373824c3326be1b8bc6f33782
Kaisum <kaisumaro445@hotmail.com>
2019-05-23 20:21:24 +0200

    tyjtytukty

--- server.js
+++ server.js
@@ -1,52 +1,67 @@
-const app = require('express')();
+const app = require("express")();
 const port = 3000;
-const bodyParser = require('body-parser');
-const axios = require('axios');
-
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const BOND_ID = "13190";
-const OSBUDDY_GR_PRICES_SUMMARY_GOOGLE_APIS = 'https://storage.googleapis.com/osbuddy-exchange/summary.json';
-const OSRS_GE_API_URL = `http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=${BOND_ID}`;
+const OSBUDDY_GR_PRICES_SUMMARY_GOOGLE_APIS_URL ="https://storage.googleapis.com/osbuddy-exchange/summary.json";
+const OSRS_GE_API_URL = `https://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=${BOND_ID}`;
+const FIREBASE_FIXED_VALS_URL = 'https://bondsflip-tool.firebaseio.com/vals.json';
 
+app.get("/summary", (req, res) => {
+    axios
+        .all([
+            axios.get(OSBUDDY_GR_PRICES_SUMMARY_GOOGLE_APIS_URL),
+            axios.get(OSRS_GE_API_URL),
+            axios.get(FIREBASE_FIXED_VALS_URL)
+        ])
+        .then(axios.spread((summary, ge_api, fixed_vals) => {
+            let fixed = fixed_vals.data;
+            let bond = summary.data[BOND_ID];
+            let convert = Math.round(
+                Number(ge_api.data.item.current.price.slice(0, -1)) * 100000
+            );
+            let isReliable = bond && bond.overall_average && convert ? true : false;
 
-app.get('/summary', (req, res) => {
-    axios.get(OSBUDDY_GR_PRICES_SUMMARY_GOOGLE_APIS)
-    .then(summary => {
-        let bond = summary.data[BOND_ID];
+            let floor = bond.overall_average - convert / 2;
+            let ceil = bond.overall_average + convert / 2;
+            let rest = bond.sell_quantity - bond.buy_quantity;
+            let factor = Number((bond.buy_quantity / bond.sell_quantity).toFixed(2));
 
-        axios.get(OSRS_GE_API_URL)
-        .then(ge_api => {
-            let convert = Math.round(Number(ge_api.data.item.current.price.slice(0, -1)) * 100000);
-            buy = bond.overall_average - (convert/2);
-            sell = bond.overall_average + (convert/2);
-            rest = bond.sell_quantity - bond.buy_quantity;
-            factor = Number((bond.buy_quantity / bond.sell_quantity).toFixed(2));
+            let buy = floor - (floor % 100000) + 6000;
+            let sell = ceil - (ceil % 100000) + 94000;
+            let margin = sell - buy - convert;
 
             res.send({
-                calc : {
-                    buy,
-                    sell,
+                calc: {
+                    sug: {
+                        buy,
+                        sell,
+                        margin
+                    },
+                    floor,
+                    ceil,
                     rest,
-                    factor
+                    factor,
+                    isReliable
                 },
+                convert,
                 bond,
-                convert
+                fixed
             });
-        })
-    })
-    .catch(err => {
-        res.send(err.message);
-    });
-})
+        }))
+        .catch(err => {
+            res.send(err.message);
+        });
+});
 
 const server = app.listen(port, () => {
     console.log(`API running on port ${port}...`);
     app.emit("appStarted");
 });
-
 
 module.exports = server;
 
